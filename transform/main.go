ae75b3dbff4be909e8d62c7a47b872d4da54ab0d
Iván Corrales Solera <developer@wesovilabs.com>
2019-01-13 04:47:54 +0100

    feat: adding new examples for groupB

--- transform/main.go
+++ transform/main.go
@@ -8,11 +8,19 @@ import (
 
 var animals = []string{"lynx", "dog", "cat", "monkey", "dog", "fox", "tiger", "lion"}
 
+type Person struct {
+	firstName string
+	age int
+}
+
 func main() {
 	fmt.Printf("input: %v\n", animals)
 	stream := koazee.StreamOf(animals)
 	fmt.Print("stream.Map(strings.Title): ")
 	fmt.Println(stream.Map(strings.Title).Do().Out().Val())
+	fmt.Print("stream.GroupBy(strings.Len): ")
+	out, _ := stream.GroupBy(func(val string)int{return len(val)})
+	fmt.Println(out)
 }
 
 /**
@@ -20,4 +28,5 @@ go run main.go
 
 input: [lynx dog cat monkey dog fox tiger lion]
 stream.Map(strings.Title): [Lynx Dog Cat Monkey Dog Fox Tiger Lion]
+stream.GroupBy(strings.Len): map[5:[tiger] 4:[lynx lion] 3:[dog cat dog fox] 6:[monkey]]
 */
